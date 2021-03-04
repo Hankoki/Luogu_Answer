503f3b92186b397badbc0c7a019b38f494971f91
Hankoki <75869701+Hankoki@users.noreply.github.com>
2021-03-04 09:59:23 +0800

    Update 超级玛丽.c

--- 超级玛丽.c
+++ 超级玛丽.c
@@ -2,13 +2,13 @@
 #include <windows.h>
 
 
-void color(short x)	
-{
-    if(x>=0 && x<=15)
-    	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);
-    else
-    	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-}
+//void color(short x)	
+//{
+  //  if(x>=0 && x<=15)
+    //	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);
+   // else
+    //	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//不需要颜色，洛谷没有windows库
+//}
 
 int main()
 {
